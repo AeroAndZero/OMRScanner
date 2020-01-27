6da8802b848db00cc9014db9a9c53d54122d8756
Ayush Thakur <ayushthakur667@gmail.com>
2020-01-27 19:44:00 +0530

    presetProcessor() in progress
    - Adding presetProcessor
    - No major changes have been made yet

--- mainframe.py
+++ mainframe.py
@@ -3,19 +3,17 @@ import tkMessageBox
 import tkFileDialog
 import aos
 import cv2
-import numpy as np
 import os
+import numpy as np
+import ast
 import shutil
 from PIL import Image, ImageTk
 
-root = tk.Tk()
+#Some Global Variables
 direction = []
 zoomFactor = 1.3
-
-'''--------- Window Customization --------'''
-root.title('AOS v0.1')
-root.geometry("900x500+100+100")
-
+actualSize = []
+batches = []
 
 '''---------------- Functions ----------------'''
 def filename(fp):
@@ -49,9 +47,6 @@ def MouseClick(event):
         direction.insert(0,[event.x,event.y])
     event.widget.bind('<B1-Motion>', onLeftDrag)
 
-def MouseEnter(event):
-    event.widget.bind("<Button-1>", MouseClick)
-
 def ImgZoomIn(canvas):
     try:
         newSize = [int(canvas.sizeX*zoomFactor),int(canvas.sizeY*zoomFactor)]
@@ -72,8 +67,6 @@ def bindZooms(canvas):
 
     zoomOut = tk.Button(canvas,text="-",width=2,command = lambda: ImgZoomOut(canvas))     #Builfing Zoom Out Button
     zoomOut.pack(side=tk.TOP,anchor=tk.N+tk.E)
-
-    #Binding Functionality
     
 
 '''---------------- Menu bar Commands ----------------'''
@@ -87,16 +80,16 @@ def openfile():
     
     #Creating temp Folder For opencv access
     try:
-        os.mkdir("temp")
+        os.mkdir(".temp")
     except OSError:
         print("Failed to make temp folder")
 
     #Copying Image to the temp folder
-    destination = os.getcwd() + "/temp/" + fn
+    destination = os.getcwd() + "/.temp/" + fn
     dest = shutil.copyfile(fp,destination)
 
     #Final File Path
-    image_path = "temp/"+str(fn)
+    image_path = ".temp/"+str(fn)
 
     # Load an color image
     img = cv2.imread(image_path)
@@ -106,6 +99,9 @@ def openfile():
         displayImage(img_fc,doCanvas,[img_fc.shape[1],img_fc.shape[0]])
     except:
         tkMessageBox.showerror("Try again","No omr was detected in the image. Please try again with a different image.")
+
+    imgCircle = aos.scanOmr(img_fc)
+    displayImage(imgCircle,doCanvas,[imgCircle.shape[1],imgCircle.shape[0]])
     
 
 def displayImage(img,canvas,size=[]):   #Displays Image On A Specific Canvas
@@ -121,77 +117,102 @@ def displayImage(img,canvas,size=[]):   #Displays Image On A Specific Canvas
     root.update()
     print("Displayed!")
         
-
 def deletetemp():
     try:
-        shutil.rmtree("temp")
+        shutil.rmtree(".temp")
     except:
         print("Couldn't delete directory temp")
     root.quit()
 
-'''-------------------- Menu bars --------------------'''
-menubar = tk.Menu(root)
-#file menu
-filemenu = tk.Menu(menubar,tearoff=0)
-filemenu.add_command(label="Open",command=openfile)
-filemenu.add_separator()
-filemenu.add_command(label="Exit",command=deletetemp)
-menubar.add_cascade(label="File",menu=filemenu)
-root.config(menu=menubar)
+def bindScrollbar(Canvas):
+    ScrollbarY = tk.Scrollbar(Canvas,orient="vertical")    #Y-Scrollbar
+    ScrollbarX = tk.Scrollbar(Canvas,orient="horizontal")   #X-Scrollbar
+    Canvas.config(yscrollcommand=ScrollbarY.set,xscrollcommand=ScrollbarX.set,yscrollincrement='1',xscrollincrement='1')    #Config Canvas For Y & X
+    ScrollbarY.config(command=Canvas.yview)         #Config Command For Scroll Y
+    ScrollbarX.config(command=Canvas.xview)         #Config Command For Scroll X
+    ScrollbarY.pack(side=tk.RIGHT,fill=tk.Y)          #Packing Scroll Y
+    ScrollbarX.pack(side=tk.BOTTOM,fill=tk.X)         #Packing Scroll X
 
-'''-----------######------------------- Main GUI -----------------######-----------'''
-root.grid_columnconfigure(0, weight=1)
-root.grid_columnconfigure(1, weight=1)
-root.grid_rowconfigure(0, weight=1)
+def main():
+    #global variables
+    global ogImgFrame,doImgFrame,ogCanvas,doCanvas,root
 
-'''--------------------- Frames --------------------'''
+    root = tk.Tk()
+    '''--------- Window Customization --------'''
+    root.title('AOS v0.1')
+    root.geometry("1000x600+100+100")
+    root.focus_force()
 
-#--------------- Original Image Frame
-ogImgFrame = tk.LabelFrame(root,text="Original Image")
-ogImgFrame.grid(row=0,column=0,sticky=tk.N+tk.S+tk.E+tk.W,padx=10,pady=5)
+    '''-------------------- Menu bars --------------------'''
+    menubar = tk.Menu(root)
+    #file menu
+    filemenu = tk.Menu(menubar,tearoff=0)
+    filemenu.add_command(label="Open",command=openfile)
+    filemenu.add_separator()
+    filemenu.add_command(label="Exit",command=deletetemp)
+    menubar.add_cascade(label="File",menu=filemenu)
+    root.config(menu=menubar)
 
-ogCanvas = tk.Canvas(ogImgFrame,height=200,width=200,scrollregion=(0,0,500,800))  #Canvas For Image
-ogScrollbarY = tk.Scrollbar(ogCanvas,orient="vertical")    #Y-Scrollbar
-ogScrollbarX = tk.Scrollbar(ogCanvas,orient="horizontal")   #X-Scrollbar
-ogCanvas.config(yscrollcommand=ogScrollbarY.set,xscrollcommand=ogScrollbarX.set,yscrollincrement='1',xscrollincrement='1')    #Config Canvas For Y & X
-ogScrollbarY.config(command=ogCanvas.yview)         #Config Command For Scroll Y
-ogScrollbarX.config(command=ogCanvas.xview)         #Config Command For Scroll X
-ogScrollbarY.pack(side=tk.RIGHT,fill=tk.Y)          #Packing Scroll Y
-ogScrollbarX.pack(side=tk.BOTTOM,fill=tk.X)         #Packing Scroll X
-ogCanvas.pack(expand=1,fill=tk.BOTH)                #Packing Canvas
+    '''-----------######------------------- Main GUI -----------------######-----------'''
+    root.grid_columnconfigure(0, weight=1)
+    root.grid_columnconfigure(1, weight=1)
+    root.grid_rowconfigure(0, weight=1)
 
-#--------------- Detected OMR Frame
-doImgFrame = tk.LabelFrame(root,text="Detected OMR")
-doImgFrame.grid(row=0,column=1,sticky=tk.N+tk.S+tk.E+tk.W,padx=5,pady=5)
+    '''--------------------- Frames --------------------'''
 
-#Detected Image Canvas Building
-doCanvas = tk.Canvas(doImgFrame,height=200,width=200,scrollregion=(0,0,500,800))    #Canvas For Detected OMR
-doScrollbarY = tk.Scrollbar(doCanvas,orient="vertical")     #Y-Scrollbar
-doScrollbarX = tk.Scrollbar(doCanvas,orient="horizontal")   #X-Scrollbar
-doCanvas.config(yscrollcommand=doScrollbarY.set,xscrollcommand=doScrollbarX.set,yscrollincrement='1',xscrollincrement='1')        #Config Canvas For Y & X             
-doScrollbarY.config(command=doCanvas.yview)             #Config Command For Scroll Y
-doScrollbarX.config(command=doCanvas.xview)             #Config Command For Scroll X
-doScrollbarY.pack(side=tk.RIGHT,fill=tk.Y)              #Packing Scroll Y
-doScrollbarX.pack(side=tk.BOTTOM,fill=tk.X)             #Packing Scroll X
-doCanvas.pack(expand=1,fill=tk.BOTH)                    #Packing Canvas
+    #--------------- Original Image Frame
+    ogImgFrame = tk.LabelFrame(root,text="Original Image")
+    ogImgFrame.grid(row=0,column=0,sticky=tk.N+tk.S+tk.E+tk.W,padx=10,pady=5)
 
+    ogCanvas = tk.Canvas(ogImgFrame,height=200,width=200,scrollregion=(0,0,500,800))  #Canvas For Image
+    bindScrollbar(ogCanvas)
+    ogCanvas.pack(expand=1,fill=tk.BOTH)                #Packing Canvas
 
-#Zoom in and zoom out buttons
-bindZooms(ogCanvas)
-bindZooms(doCanvas)
+    #--------------- Detected OMR Frame
+    doImgFrame = tk.LabelFrame(root,text="Detected OMR")
+    doImgFrame.grid(row=0,column=1,sticky=tk.N+tk.S+tk.E+tk.W,padx=5,pady=5)
 
-#--------------- Settings Panel
-settingsFrame = tk.LabelFrame(root,text="Settings")
-settingsFrame.grid(row=0,column=2,sticky=tk.N+tk.S+tk.E+tk.W,padx=5,pady=5)
-
-testL = tk.Label(settingsFrame,text="Settings & Export Options Will Be Added Here")
-testL.pack()
+    doCanvas = tk.Canvas(doImgFrame,height=200,width=200,scrollregion=(0,0,500,800))    #Canvas For Detected OMR
+    bindScrollbar(doCanvas)
+    doCanvas.pack(expand=1,fill=tk.BOTH)                    #Packing Canvas
 
 
-#-------------- Button Clicking
-ogCanvas.bind("<Enter>", MouseEnter)
-doCanvas.bind("<Enter>", MouseEnter)
+    #Zoom in and zoom out buttons
+    bindZooms(ogCanvas)
+    bindZooms(doCanvas)
 
-'''---------------------------- Exit Commands ----------------------------'''
-root.protocol('WM_DELETE_WINDOW', deletetemp)
-root.mainloop()
+    #--------------- Settings Panel
+    settingsFrame = tk.LabelFrame(root,text="Settings")
+    settingsFrame.grid(row=0,column=2,sticky=tk.N+tk.S+tk.E+tk.W,padx=5,pady=5)
+
+    testL = tk.Label(settingsFrame,text="Settings & Export Options Will Be Added Here")
+    testL.pack()
+
+    #-------------- Button Clicking
+    ogCanvas.bind("<Button-1>", MouseClick)
+    doCanvas.bind("<Button-1>", MouseClick)
+
+    '''---------------------------- Exit Commands ----------------------------'''
+    root.protocol('WM_DELETE_WINDOW', deletetemp)
+    root.mainloop()
+
+def presetProcessor(presetString):
+    global batches,presetName,presetDescription,presetCE1,presetCE2,presetBlur,actualSize
+    try:
+        presetData = presetString.split("$")
+        presetName = presetData[0]
+        presetDescription = presetData[1]
+        actualSize = ast.literal_eval(presetData[2])
+        presetCE1 = int(presetData[3])
+        presetCE2 = int(presetData[4])
+        presetBlur = int(presetData[5])
+        presetTotalBatches = int(presetData[6])
+        for batchIndex in range(7,7+presetTotalBatches,1):
+            batches.append(ast.literal_eval(presetData[batchIndex]))
+        print(batches)
+    except Exception as e:
+        tkMessageBox.showerror("Corrupted Preset","The preset you are trying to use is corrupted. Re-make the preset or use another one.")
+        print(e)
+
+if __name__ == "__main__":
+    main()
